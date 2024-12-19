3999bbe174788e37cbd5cdfac8f749d74afcdf24
RoanKanninga <roankanninga@gmail.com>
2024-12-19 12:40:27 +0100

    fixing concordanceCheckVcf header

--- protocols/ReheaderVCF.sh
+++ protocols/ReheaderVCF.sh
@@ -13,18 +13,23 @@ set -o pipefail
 #string projectResultsDir
 #string capturingKit
 #string dataDir
+#string concordanceCheckCallsVcf
 
 module load "${bcfToolsVersion}"
 module load "${htsLibVersion}"
 
 ##new prefix will be externalSampleID + sampleProcessStepIDwith new sample identifier --> familyname+umcgnumber
 newGVCFSampleIdentifier=$(echo "${externalSampleID}" | awk 'BEGIN {FS="_"}{print $1"_"$2}')
+
 oldIdentifier=$(cat "${intermediateDir}/${externalSampleID}.txt")
 echo -e "old:${oldIdentifier}, new:${newGVCFSampleIdentifier}"
 echo -e "${oldIdentifier} ${newGVCFSampleIdentifier}" > "${intermediateDir}/${externalSampleID}.newVCFHeader.txt"
+echo -e "${oldIdentifier} ${externalSampleID}" > "${intermediateDir}/${externalSampleID}.newConcordanceCheckHeader.txt"
 
 echo "##intervals=[${dataDir}/${capturingKit}/human_g1k_v37/captured.merged.bed]" > "${intermediateDir}/bedfile.txt"
 bcftools reheader -s "${intermediateDir}/${externalSampleID}.newVCFHeader.txt" "${gavinOutputFinalMergedRLV}.gz" -o "${gavinOutputFinalMergedRLV}.gz.tmp"
+bcftools reheader -s "${intermediateDir}/${externalSampleID}.newConcordanceCheckHeader.txt" "${concordanceCheckCallsVcf}" -o "${concordanceCheckCallsVcf}.tmp"
+mv "${concordanceCheckCallsVcf}.tmp" "${concordanceCheckCallsVcf}"
 bcftools annotate -h "${intermediateDir}/bedfile.txt" -O v -o "${gavinOutputFinalMergedRLV}.tmp.vcf.gz" "${gavinOutputFinalMergedRLV}.gz.tmp"
 mv -v "${gavinOutputFinalMergedRLV}.tmp.vcf.gz" "${gavinOutputFinalMergedRLV}.gz"
 tabix -f -p vcf "${gavinOutputFinalMergedRLV}.gz"
